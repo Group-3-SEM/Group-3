cfa09c0f6843b562213ec0459605b1259873a15e
Jack2680 <jcjohnson2680@gmail.com>
2021-03-05 10:52:55 +0000

    adding country

---
+++ src/main/java/Group3/Country.java
@@ -0,0 +1,81 @@
+package Group3;
+/**
+ * Represents an country
+ */
+public class Country
+{
+    /**
+     * country code
+     */
+    public String Code;
+
+    /**
+     * Country name
+     */
+    public String Name;
+
+    /**
+     * Continent
+     */
+    public String Continent;
+
+    /**
+     *  Region
+     */
+    public String Region;
+
+    /**
+     * Surface Area
+     */
+    public float SurfaceArea;
+
+    /**
+     * inDep Year
+     */
+    public int IndepYear;
+
+    /**
+     * population
+     */
+    public int population;
+
+    /**
+     * LifeExpectancy
+     */
+    public float LifeExpectancy;
+
+    /**
+     * GNP
+     */
+    public float GNP;
+
+    /**
+     * Old GNP
+     */
+    public float OldGNP;
+
+    /**
+     * Local Name
+     */
+    public String LocalName;
+
+    /**
+     * Government Form
+     */
+    public String GovernmentForm;
+
+    /**
+     * HeadOfState
+     */
+    public String HeadOfState;
+
+    /**
+     * Capital
+     */
+    public int Capital;
+
+    /**
+     * Code2
+     */
+    public int Code2;
+}
