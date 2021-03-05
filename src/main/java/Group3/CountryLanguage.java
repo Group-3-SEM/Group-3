b62ab2f24a2857ec2ef78a0e072a8038697d970c
Jack2680 <jcjohnson2680@gmail.com>
2021-03-05 11:00:54 +0000

    adding CountryLanguage class

---
+++ src/main/java/Group3/CountryLanguage.java
@@ -0,0 +1,24 @@
+package Group3;
+
+public class CountryLanguage {
+
+    /**
+     * country code
+     */
+    public String CountryCode;
+
+    /**
+     * Language
+     */
+    public String Language;
+
+    /**
+     * IsOfficial
+     */
+    public String IsOfficial;
+
+    /**
+     * Percentage
+     */
+    public float Percentage;
+}
