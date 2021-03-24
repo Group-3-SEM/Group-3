2cac71d26371db1a929698b5284afaed131ecb6f
KianDF <78480032+KianDF@users.noreply.github.com>
2021-03-24 09:04:02 +0000

    Update App.java

--- src/main/java/Group3/App.java
+++ src/main/java/Group3/App.java
@@ -41,7 +41,7 @@ public class App {
         a.displayCountry(report28);
         
 	//Display report 24    
-	ArrayList<Country> report24 = a.getReport24("Africa");
+	ArrayList<Country> report24 = a.getReport24();
 	a.printCountries(report24);
 	    
         // Disconnect from database
